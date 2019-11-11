59aa41a5c561c10bf1c0a1c6590656543efcf772
Daniel Rolnik <danrol92@gmail.com>
2019-11-11 11:11:06 +0200

    upd

--- boolean_conjunction_predictor.py
+++ boolean_conjunction_predictor.py
@@ -7,7 +7,6 @@ file_path = "data.txt"
 #         all_ones_hyp = list(range(size))
 #         all_zeros_hyp = list(range(size))
 
-
 def get_all_negative_hypothesis(train_data_size):
     h = []
     for index in range(1, train_data_size):
@@ -17,44 +16,47 @@ def get_all_negative_hypothesis(train_data_size):
 
 def get_y_predict(train_data_row, h):
     result = 1
-    sign = 1
-    for index in range(1, len(h)):
-        if h[index]> 0:
-            sign = 1
-        elif h[index] < 0:
-            sign = -1
-        result = result and (sign*train_data_row[abs(h[index])])
+    for index in range(len(h)):
+        data_literal_index = abs(h[index])
+        data_literal_value = train_data_row[data_literal_index-1]
+        if h[index] < 0:
+            if data_literal_value == 0:
+                data_literal_value = 1
+            else:
+                data_literal_value = 0
+        result = result and data_literal_value
     return result
 
 
 def consistency_alg(train_data):
-    # xt is training data
     h_previous = get_all_negative_hypothesis(len(train_data[0]))
     print("h after received: ")
     print(h_previous)
     num_of_examples = len(train_data)
-    num_of_literals = len(train_data[0])-1
     y = train_data[:, len(train_data[0])-1]
 
     for instance_t in range(num_of_examples):
-        h = h_previous
+        h = h_previous.copy()
         yt = y[instance_t]
-        y_predict = get_y_predict(train_data[instance_t], h)
+        x_row = train_data[instance_t]
+        y_predict = get_y_predict(x_row[0: -1], h)
         if (yt == 1) and (y_predict == 0):
             print("entered if")
-            for index in range(1, instance_t+1):
+            for index in range(instance_t+1):
+                literal_serial_num = index+1
                 x_iter = train_data[instance_t][index]
                 if x_iter == 1:
-                    h.remove(-index)
+                    h.remove(-literal_serial_num)
                     #print("iteration = "+instance_t+" removing -index = "+(-index))
                 if x_iter == 0:
-                    h.remove(index)
+                    h.remove(literal_serial_num)
                     #print("iteration = "+instance_t+" removing -index = "+ index)
         if yt == 0:
             continue
-        h_previous = h
+        h_previous = h.copy()
     return h
 
 if __name__ == "__main__":
     training_examples = np.loadtxt(file_path)
+    print("final answer:")
     print(consistency_alg(training_examples))
