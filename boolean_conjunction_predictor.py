f206144de144172cf172841b612fb80ef5afc7d7
Daniel Rolnik <danrol92@gmail.com>
2019-11-11 12:09:18 +0200

    final

--- boolean_conjunction_predictor.py
+++ boolean_conjunction_predictor.py
@@ -1,11 +1,8 @@
 import numpy as np
-file_path = "data.txt"
+input_file_path = "data.txt"
+#input_file_path = "alternative_data.txt"
+output_file_path = "output.txt"
 
-
-# class Hypothesis:
-#     def __init__(self, size):
-#         all_ones_hyp = list(range(size))
-#         all_zeros_hyp = list(range(size))
 
 def get_all_negative_hypothesis(train_data_size):
     h = []
@@ -30,33 +27,48 @@ def get_y_predict(train_data_row, h):
 
 def consistency_alg(train_data):
     h_previous = get_all_negative_hypothesis(len(train_data[0]))
-    print("h after received: ")
-    print(h_previous)
     num_of_examples = len(train_data)
     y = train_data[:, len(train_data[0])-1]
 
     for instance_t in range(num_of_examples):
         h = h_previous.copy()
         yt = y[instance_t]
-        x_row = train_data[instance_t]
-        y_predict = get_y_predict(x_row[0: -1], h)
+        x_row = train_data[instance_t][:-1]
+        y_predict = get_y_predict(x_row, h)
         if (yt == 1) and (y_predict == 0):
-            print("entered if")
-            for index in range(instance_t+1):
-                literal_serial_num = index+1
-                x_iter = train_data[instance_t][index]
+
+            for index, x_iter in enumerate(x_row):
+                literal_serial_num_to_del = index + 1
                 if x_iter == 1:
-                    h.remove(-literal_serial_num)
-                    #print("iteration = "+instance_t+" removing -index = "+(-index))
-                if x_iter == 0:
-                    h.remove(literal_serial_num)
-                    #print("iteration = "+instance_t+" removing -index = "+ index)
+                    literal_serial_num_to_del = -literal_serial_num_to_del
+                if literal_serial_num_to_del in h:
+                    h.remove(literal_serial_num_to_del)
+
         if yt == 0:
             continue
         h_previous = h.copy()
     return h
 
+
+def get_string_result(h):
+    result = ""
+    for x in h:
+        if x < 0:
+            result = result + "not(x" + str(abs(x))+") "
+        else:
+            result = result + "x"+str(abs(x))+" "
+    return result
+
+
+def write_output_to_file(h, result):
+    file = open(output_file_path, 'w')
+    file.write(result)
+    file.close()
+
+
 if __name__ == "__main__":
-    training_examples = np.loadtxt(file_path)
-    print("final answer:")
-    print(consistency_alg(training_examples))
+    training_examples = np.loadtxt(input_file_path)
+    h = consistency_alg(training_examples)
+    string_result = get_string_result(h)
+    write_output_to_file(h, string_result)
+    print(string_result)
